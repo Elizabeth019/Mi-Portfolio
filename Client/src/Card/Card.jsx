0029113e3c2d767660831fd6e4a9754087940a91
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-09-23 14:55:51 -0300

    Cards implementadas

--- Client/src/Card/Card.jsx
+++ Client/src/Card/Card.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import style from './Card.module.css';
+import stands from '../datos';
+
+//import { Link } from '@chakra-ui/react';
 
 export default function Card() {
+    return (
+   //      <div className={style.central}>
+      <div className={style.general}>
+{stands.map((stand) => {
    return (
-        <div className={style.central}>
-      <div className={style.container}>
-         <h4>quimica</h4>
-         {/* Corrige la ruta de la imagen y el atributo alt */}
-         <img src="../../asset/image/fotos-quimica-stands/IMG-20230815-WA0137.jpg" alt='Imagen de química' />
-</div>
+      <div key={stand.id} className={style.container}>
+         <h4>{stand.title}</h4>
+        
+        <img src={stand.image} alt='Imagen de química' />
+         
+         </div>
+)})}
+
+         {/* <h4>quimica</h4>
+        
+         <img src="../../asset/image/fotos-quimica-stands/IMG-20230815-WA0137.jpg" alt='Imagen de química' /> */}
+{/* </div>
 <div className={style.container}>
          <h4>quimica</h4>
-         {/* Corrige la ruta de la imagen y el atributo alt */}
-         <img src="../../asset/image/fotos-quimica-stands/IMG-20230815-WA0137.jpg" alt='Imagen de química' />
+        
+         <img src="../../asset/image/fotos-quimica-stands/IMG-20230815-WA0004.jpg" alt='Imagen de química' /> */}
       </div>
-      </div>
+      // </div>
    );
 }
