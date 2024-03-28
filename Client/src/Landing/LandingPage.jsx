0ef10739789924682c4c6eea748d704986335bf2
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-03-28 15:40:28 -0300

    background modificado

--- Client/src/Landing/LandingPage.jsx
+++ Client/src/Landing/LandingPage.jsx
@@ -1,84 +1,9 @@
-//  import React from "react";
-// import style from "./LandingPage.module.css";
-// export default function LandingPage() {
-//   return (
-//     <div className={style.container}>
-    
-// <img src="./asset/image/fotos-quimica-stands\IMG-20230815-WA0043.jpg" alt="logo" className={style.transparencia}/>
-
-//       <div className={style.logo}>
-
-//       <img src="./asset/image/quimica-removebg-preview.png" alt="logo" className={style.image}/>
-
-//       <button>
-//   <a href="/inicio" className={style.btn2}><span class="spn2">HELLO !</span></a>
-// </button>
-// </div>
-//     </div>
-//   )};
-
-// //  import { useState } from 'react';
-// // import { Box, Button, useTheme } from '@chakra-ui/react';
-// // import { Image } from '@chakra-ui/react';
-// // import datos from '../datos.js';
-
-// // const Carousel = () => {
-// //   const [activeStep, setActiveStep] = useState(0);
-// //   const maxSteps = datos.length;
-// //   const theme = useTheme();
-
-// //   const handleNext = () => {
-// //     setActiveStep((prevActiveStep) => (prevActiveStep === maxSteps - 1 ? 0 : prevActiveStep + 1));
-// //   };
-
-// //   const handleBack = () => {
-// //     setActiveStep((prevActiveStep) => (prevActiveStep === 0 ? maxSteps - 1 : prevActiveStep - 1));
-// //   };
-
-// //   return (
-// //     <Box maxW="400px" flexGrow="1">
-// //       <Image
-// //         src={datos[activeStep].image}
-// //         alt={`Slide ${activeStep + 1}`}
-// //         width="100%"
-// //         height="auto"
-// //         objectFit="cover"
-// //       />
-// //       <Box display="flex" justifyContent="center" mt={4}>
-// //         <Button size="sm" onClick={handleBack} mr={2}>
-// //           Back
-// //         </Button>
-// //         <Button size="sm" onClick={handleNext}>
-// //           Next
-// //         </Button>
-// //       </Box>
-// //     </Box>
-// //   );
-// // };
-
-// // export default Carousel;
-
-
-// import React, { useState } from "react";
-// import style from "./LandingPage.module.css";
-// import { Box, Button, useTheme } from '@chakra-ui/react';
-// import { Image } from '@chakra-ui/react';
-
-// export default function LandingPage() {
-//   // Definir el array de imágenes
-//   const images = [
-//     "./asset/image/fotos-quimica-stands/IMG-20230815-WA0043.jpg",
-//     "./asset/image/fotos-quimica-stands/IMG-20230815-WA0064.jpg",
-//     "./asset/image/fotos-quimica-stands/IMG-20230815-WA0112.jpg"
-//   ];
-
   'use client'
-
-  import React, { useState } from 'react';
+import React, { useState } from 'react';
   import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
   import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
   import Slider from 'react-slick';
-  import style from "./LandingPage.module.css"; // Importa tu archivo de estilos
+  import style from "./LandingPage.module.css";
   
   const LandingPage = () => {
     const [slider, setSlider] = useState(null);
@@ -113,7 +38,7 @@
     };
   
     return (
-      <Box position="relative" height="633px" width="full" overflow="hidden">
+      <Box position="relative" height="633px" width="full" overflow="hidden"  backgroundColor= "#212121" >
        
        <link
           rel="stylesheet"
