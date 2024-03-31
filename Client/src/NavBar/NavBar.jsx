e8d406db435e5833ecc927d253426e4088056021
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-03-31 07:10:40 -0300

    navbar modificado

--- Client/src/NavBar/NavBar.jsx
+++ Client/src/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import ToggleColorMode from "../modo/ThemeContext";
 import { useTheme } from '../modo/ThemeProvider';
 import { BiSun, BiMoon } from 'react-icons/bi';
 
