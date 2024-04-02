523ffedcd3a0d87c33fb69a4946a2e622ca4a386
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-04-01 23:54:26 -0300

    Comenzando pagina de inicio

--- Client/src/Home/Home.jsx
+++ Client/src/Home/Home.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import Card from "../Card/Card";
 import style from "./Home.module.css";
 import 'animate.css';
-import 'atropos/css';
-//import Atropos from 'atropos/react';
 function Home() {
   return (
-    <div className={style.container}>
-
-      <h1>¡¡Bienvenidos a nuestra página	web!! </h1>
+    < div className={style.container}>
       {/* <hr></hr> */}
-      {/* <Atropos className="my-atropos"></Atropos> */}
+      <h1 className={style.title}>¡¡Bienvenidos a nuestra página	web!! </h1>
+     
       <div className={style.cards}>
       <Card />
       </div>
