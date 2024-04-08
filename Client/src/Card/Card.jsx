7cd86adbb96a48b0712f1c4a8f04c3475582dbcf
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-04-08 09:21:09 -0300

    renderizado de imagenes

--- Client/src/Card/Card.jsx
+++ Client/src/Card/Card.jsx
@@ -1,37 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, Button, Image } from '@chakra-ui/react';
+import { getAllPhotos } from '../Redux/Actions/actions';
+//import  {photos } from '../Redux/reducer'
 import stands from '../datos';
-//import { Link } from 'react-router-dom';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
 
-export default function Card() {
+const itemsPerPage = 16; // Cantidad de imágenes por página
+
+export default function PaginatedImageList() {
+  const dispatch = useDispatch();
+const imageData = useSelector(state => state.photos);
+console.log(imageData)
+const [page, setPage] = useState(0);
+
+useEffect(() => {
+  dispatch(getAllPhotos());
+}, [dispatch]);
+
+const handleNextPage = () => {
+  setPage(prevPage => prevPage + 1);
+};
+
+const handlePrevPage = () => {
+  setPage(prevPage => prevPage - 1);
+};
+
+const startIndex = page * itemsPerPage;
+const endIndex = startIndex + itemsPerPage;
+const paginatedItems = imageData && imageData.slice(startIndex, endIndex);
+//const paginatedItems = stands.slice(startIndex, endIndex);
+
+
+
   return (
-   //  <ImageList sx={{ width: 500, height: 450 }} variant="quilted" cols={4} rowHeight={121}>
-   //    {stands.map((stand) => (
-   //      <ImageListItem key={stand.id} cols={1} rows={1}>
-   //        <img src={stand.image} alt='Imagen de química' />
-          
-   //      </ImageListItem>
-   //    ))}
-   //  </ImageList>
-
-   <ImageList
-  sx={{ width: 500, height: 450 }}
-  variant="quilted"
-  cols={4}
-  rowHeight={121}
->
-  {stands.map((item) => (
-    <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-     <img
-  src={item.img}
-  srcSet={`${item.img} 1x, ${item.img2x} 2x`}
-  alt={item.title}
-  loading="lazy"
-/>
-    </ImageListItem>
-  ))}
-</ImageList>
+    <Box maxW="1300px" margin="0 auto">
+      <Box display="grid" gridTemplateColumns="repeat(4, 1fr)" gap={4}>
+        {paginatedItems.map((stand, index) => (
+          <Box key={index}>
+            <Image src={stand.url} alt={stand.title} boxSize="300px" />
+            {/* <Image src={stand.image} alt={stand.title} boxSize="300px" /> */}
+          </Box>
+        ))}
+      </Box>
+      <Box textAlign="center" marginTop="10px">
+        <Button variant="contained" onClick={handlePrevPage} disabled={page === 0}>
+          Anterior
+        </Button>
+        <Button variant="contained" onClick={handleNextPage} disabled={endIndex >= stands.length}>
+          Siguiente
+        </Button>
+      </Box>
+    </Box>
   );
 }
