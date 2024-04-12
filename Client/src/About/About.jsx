d123d2d98dbd4388c08c2588328959d7ff056882
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-04-12 09:45:09 -0300

    cambios en about

--- Client/src/About/About.jsx
+++ Client/src/About/About.jsx
@@ -1,67 +1,29 @@
-// import React from "react";
-// import style from "./About.module.css";
-
-// function About (){
-// return(
-// <div className={style.container}>
-// <h2 className={style.title}>Acerca de Nosotros</h2>
-//   <hr></hr>
-//   <img className={style.img} src="./asset/istockphoto-463196047-1024x1024-transformed.jpeg" alt='Imagen de química' />
-
-
-//  <p className="paragraph">
-// En el corazón de nuestra historia se encuentra el espíritu emprendedor de tres amigos y socios que compartieron un sueño audaz: crear una empresa que marque la diferencia. Con pasión, dedicación y un compromiso inquebrantable, nos lanzamos a la emocionante travesía empresarial, sin saber exactamente dónde nos llevaría el camino.</p>
-
-// <p className="paragraph">
-// Nuestro viaje comenzó con un sueño en mente y un pequeño equipo en el garaje de uno de nosotros. Las noches se convertían en días, y los días en noches, mientras trabajábamos incansablemente para transformar nuestra visión en realidad. Los obstáculos se presentaban en cada esquina, pero nuestra determinación no flaqueó. Sabíamos que cada desafío era una oportunidad disfrazada, y estábamos dispuestos a enfrentarlos todos.
-
-// Con el tiempo, nuestro esfuerzo constante comenzó a dar sus frutos. Lo que una vez fue un pequeño garaje se convirtió en una oficina modesta y, finalmente, en un espacio de trabajo vibrante y lleno de vida. A medida que nuestro equipo crecía, también lo hacía nuestra pasión por ofrecer productos y servicios excepcionales.
-
-// Hoy, miramos hacia atrás y celebramos el viaje que nos ha llevado hasta aquí. Hemos construido relaciones sólidas con nuestros clientes, quienes confían en nosotros para brindar soluciones innovadoras y de calidad. Hemos aprendido que el éxito no se mide solo en términos de ingresos, sino en el impacto que generamos en las vidas de las personas y en las comunidades que servimos.
-// </p>
-
-// <h3 className="subtitle">Nuestros Valores</h3>
-// Nuestro compromiso con la excelencia, la ética y el servicio al cliente sigue siendo la base de nuestra empresa. Siempre buscamos la oportunidad de crecer, aprender y superar nuestras propias expectativas. Somos conscientes de que cada día es un nuevo capítulo en nuestra historia, y lo enfrentamos con gratitud y humildad.
-// <ul className="list">
-//     <li>Compromiso con la excelencia</li>
-//     <li>Ética empresarial</li>
-//     <li>Atención al cliente</li>
-//   </ul>
-//   <p className="paragraph">
-// Agradecemos a todos aquellos que han sido parte de nuestro viaje: nuestros leales clientes, nuestros talentosos colaboradores y nuestras familias que nos han apoyado en cada paso. Sin ustedes, nuestro sueño no sería posible.
-// </p>
-// El camino puede haber sido desafiante, pero cada desafío nos ha convertido en lo que somos hoy: una empresa apasionada, comprometida con la excelencia y con una visión audaz para el futuro. Estamos emocionados por lo que vendrá a continuación y esperamos seguir siendo una parte valiosa de su vida y su éxito.
-
-// <a href="#" className="link">Más información</a>
-// <hr></hr>
-//  <h3>¡Gracias por ser parte de nuestra historia!
-
-// Atentamente,
-
-// Quimica Stands.</h3>
-
-
-//   </div>
-// )
-// };
-
-// export default About;
-import React from 'react'
+import React, { useEffect, useRef} from 'react'
 //import style from './About.module.css'
 //import { Link } from '@chakra-ui/react'
 import { Link } from "react-router-dom";
-//import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-//import { Heading, Image, Stack, Text, Button } from '@chakra-ui/react'
 import fotoImage from '../../asset/image/fotos-quimica-stands/IMG-20230815-WA0109.jpg'
 import {
   Box,
   Button,
   Flex, SimpleGrid, chakra, Image
 } from "@chakra-ui/react";
+import Contacto from '../Contacto/contacto';
 
 export default function About() {
-
-
+  const contactRef = useRef(null);
+  // const scrollToContact = () => {
+  //   const currentPath = window.location.pathname;
+  
+  //   if (currentPath !== '/nosotros') {
+  //     window.location.href = '/nosotros#contactos';
+  //   } else {
+  //     if (contactRef.current) {
+  //       contactRef.current.scrollIntoView({ behavior: 'smooth' });
+  //     }
+  //   }
+  // };
+  
  return (
 <>
 
@@ -86,6 +48,7 @@ export default function About() {
       _dark={{ color: "gray.300" }}
       lineHeight="shorter"
       textShadow="2px 2px 3px rgba(0, 0, 0, 0.5)"
+      marginBottom={"1rem"}
     >
       Nuestra Historia
     </chakra.h1>
@@ -98,7 +61,7 @@ export default function About() {
     >
       En el corazón de nuestra historia se encuentra el espíritu emprendedor de tres amigos y socios que compartieron un sueño audaz: crear una empresa que marque la diferencia. Con pasión, dedicación y un compromiso inquebrantable, nos lanzamos a la emocionante travesía empresarial, sin saber exactamente dónde nos llevaría el camino.
     </chakra.p>
-    <br></br>
+  
     <chakra.p
       fontSize="lg"
       color="brand.600"
@@ -121,20 +84,10 @@ export default function About() {
 
 </chakra.p>
 
-<br></br>
-    <chakra.p
-      fontSize="lg"
-      color="brand.600"
-      _dark={{ color: "gray.400" }}
-      letterSpacing="wider"
-      textAlign="justify"
-    >
- Hoy, miramos hacia atrás y celebramos el viaje que nos ha llevado hasta aquí. Hemos construido relaciones sólidas con nuestros clientes, quienes confían en nosotros para brindar soluciones innovadoras y de calidad. Hemos aprendido que el éxito no se mide solo en términos de ingresos, sino en el impacto que generamos en las vidas de las personas y en las comunidades que servimos.
-    
-    </chakra.p>
+{/* 
     <Link to="/contacto">
       <Button variant="boton1">Conócenos</Button>
-    </Link>
+    </Link> */}
   </Box>
 </Flex>
 
@@ -163,6 +116,20 @@ export default function About() {
             md: 20,
           }}
         >
+
+    <chakra.p
+      fontSize="lg"
+      color="white"
+      _dark={{ color: "gray.400" }}
+      letterSpacing="wider"
+      textAlign="justify"
+    >
+ Hoy, miramos hacia atrás y celebramos el viaje que nos ha llevado hasta aquí. Hemos construido relaciones sólidas con nuestros clientes, quienes confían en nosotros para brindar soluciones innovadoras y de calidad. Hemos aprendido que el éxito no se mide solo en términos de ingresos, sino en el impacto que generamos en las vidas de las personas y en las comunidades que servimos.
+    
+    </chakra.p>
+    
+<br></br>
+
           <chakra.h2
             fontSize={{
               base: "3xl",
@@ -176,6 +143,7 @@ export default function About() {
             }}
             mb={6}
           >
+
             <chakra.span
               mb={4}
               fontSize={{
@@ -194,6 +162,7 @@ export default function About() {
             >
               Y además . . .
             </chakra.span>
+            
             <chakra.span
               display="block"
               mb={4}
@@ -235,13 +204,12 @@ export default function About() {
      <li>Atención al cliente</li>
    </ul>
           </chakra.p>
-          <Link to="/home">
-            <Button border={"1px"} variant="boton1">
-              Ir a Compras
-            </Button>
-          </Link>
+        
         </Box>
       </Flex>
+          <section id="contacto" ref={contactRef}>
+<Contacto/>
+</section>
      
 </>
 ) 
