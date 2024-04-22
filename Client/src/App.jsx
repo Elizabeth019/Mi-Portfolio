d49583bc1eb47bb3bd1b739b61c5ec417c8a0879
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-04-22 17:59:15 -0300

    app

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import {Routes, Route, useLocation} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './NavBar/NavBar';
 import LandingPage from './Landing/LandingPage';
 import Home from './Home/Home';
-//import Detail from './Detail/Detail';
 import About from './About/About';
+import Contacto from './Contacto/contacto';
+import Form from './Image_Form/image_form';
+import Servicios from './Servicios/Servicios';
 
 const App = () => {
   const location = useLocation();
+
   return (
-    <div>
-      {location.pathname !== "/" && <Navbar />}
-    <Routes>
-      
-      <Route path='/' element={<LandingPage/>}/>
-        <Route  path="/inicio" element={<Home/>} />
-        {/*<Route path= "/stand/:id" element={<Detail/>} />*/}
-         <Route path="/nosotros" element={<About/>} /> 
-    </Routes>
-    </div>
+    
+    <>
+
+      {location.pathname !== '/' && <Navbar />}
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/inicio' element={<Home />} />
+        {/*<Route path="/stand/:id" element={<Detail/>} />*/}
+        <Route path="/servicios" element={<Servicios/>} />
+        <Route path='/nosotros' element={<About />} />
+        <Route path='/contacto' element={<Contacto />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
+   
+    </>
   );
 };
 
